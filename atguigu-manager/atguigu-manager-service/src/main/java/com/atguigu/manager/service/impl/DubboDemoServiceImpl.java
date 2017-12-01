5ce8006e188871f676c3d1c93489109d681c2f6f
fewcold <huaziplszl@gmail.com>
2017-12-01 20:14:48 +0800

    完成服务发布模块

--- atguigu-manager/atguigu-manager-service/src/main/java/com/atguigu/manager/service/impl/DubboDemoServiceImpl.java
+++ atguigu-manager/atguigu-manager-service/src/main/java/com/atguigu/manager/service/impl/DubboDemoServiceImpl.java
@@ -1,8 +1,9 @@
 package com.atguigu.manager.service.impl;
 
 import com.atguigu.manager.service.DubboDemoService;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class DubboDemoServiceImpl implements DubboDemoService {
     @Override
     public String hello(String name) {
